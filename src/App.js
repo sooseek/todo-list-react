383cda52006df7a21b82257a5df8233eac500cda
Mateusz Suskiewicz <mateusz.suskiewicz@gmail.com>
2022-05-26 10:24:17 +0200

    added setAllDone interaction

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ function App() {
     );
   };
 
+  const setAllDone = () => {
+    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -49,6 +53,7 @@ function App() {
             tasks={tasks}
             hideDone={hideDone}
             toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
           />
         }
       />
