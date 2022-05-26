a02f1aceebfb2c4725edb1d1d10c742176faafbb
Mateusz Suskiewicz <mateusz.suskiewicz@gmail.com>
2022-05-26 09:05:21 +0200

    added key in Tasks section

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const tasks = [
   { id: 2, content: "zrobić obiad", done: true },
 ];
 
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
   return (
@@ -20,10 +20,8 @@ function App() {
       <Section title={"Dodaj nowe zadanie"} body={<Form />} />
       <Section
         title={"Lista zadań"}
-        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
-        extraHeaderContent={
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        }
+        body={<Tasks tasks={tasks} hideDone={hideDone} />}
+        extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
       />
     </Container>
   );
